9762d95786f915a615e1841c5855ae3e777b40d1
txtranslation <transifex.translator@gmail.com>
2014-12-27 22:08:22 +0000

    [tool] adjust script

--- tool/sync-scripts/sync_main.sh
+++ tool/sync-scripts/sync_main.sh
@@ -1,7 +1,8 @@
 cd ../..
+echo "Starting download phase"
 xbmc-txupdate translations/xbmc-main/
 while true; do
-    read -p "Please in another terminal check if all went fine! Can we continue? " yn
+    read -p "Please in another terminal check if all went fine to start upload to TX! Can we continue? (y/n)" yn
     case $yn in
         [Yy]* ) break;;
         [Nn]* ) exit;;
@@ -9,10 +10,11 @@ while true; do
     esac
 done
 
+echo "Starting upload phase"
 xbmc-txupdate translations/xbmc-main/ -u
 
 while true; do
-    read -p "Please check again if all went fine and we can commit changes! Can we continue? " yn
+    read -p "Please check again if all went fine and we can commit changes! Can we continue? (y/n)" yn
     case $yn in
         [Yy]* ) break;;
         [Nn]* ) exit;;
@@ -20,8 +22,11 @@ while true; do
     esac
 done
 
+echo -e "git add -A\n"
 git add -A
+echo -e "git commit -am [xbmc-main] sync\n"
 git commit -am "[xbmc-main] sync"
+echo -e "git push \"origin master\"\n"
 git push origin master
 
 cd tool/sync-scripts
