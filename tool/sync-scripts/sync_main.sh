4206a07bd3218daa1923e2b38c8ee3b976006c65
txtranslation <transifex.translator@gmail.com>
2014-12-28 16:50:24 +0000

    [tool] adjust sync script

--- tool/sync-scripts/sync_main.sh
+++ tool/sync-scripts/sync_main.sh
@@ -1,6 +1,20 @@
 cd ../..
-echo "Starting download phase"
-xbmc-txupdate translations/xbmc-main/
+
+echo -e "\nPlease choose which project to sync:"
+select yn in "xbmc-main" "xbmc-skins" "xbmc-addons"; do
+    case $yn in
+        xbmc-main )   PROJECT=xbmc-main;    break;;
+        xbmc-skins )  PROJECT=xbmc-skins;   break;;
+        xbmc-addons ) PROJECT=xbmc-addons;  break;;
+    esac
+done
+
+echo "Starting phase I."
+echo "Now we download the language files from upstream loacations and Transifex."
+echo "We merge them and check if there is something we should upload to Transifex later.\n"
+sleep 3
+
+xbmc-txupdate translations/$PROJECT/
 while true; do
     read -p "Please in another terminal check if all went fine to start upload to TX! Can we continue? (y/n)" yn
     case $yn in
@@ -10,8 +24,11 @@ while true; do
     esac
 done
 
-echo "Starting upload phase"
-xbmc-txupdate translations/xbmc-main/ -u
+echo "Starting phase II."
+echo "Now we upload the new strings to transifex which are coming from upstream locations.\n"
+sleep 3
+
+xbmc-txupdate translations/$PROJECT/ -u
 
 while true; do
     read -p "Please check again if all went fine and we can commit changes! Can we continue? (y/n)" yn
@@ -22,11 +39,15 @@ while true; do
     esac
 done
 
+echo "Starting phase III."
+echo "Now we commit changes to the Kodi-Translations github project and push them."
+sleep 3
+
 echo -e "git add -A\n"
 git add -A
-echo -e "git commit -am [xbmc-main] sync\n"
-git commit -am "[xbmc-main] sync"
-echo -e "git push \"origin master\"\n"
+echo -e "git commit -am [$PROJECT] sync\n"
+git commit -am "[$PROJECT] sync"
+echo -e "\ngit push \"origin master\"\n"
 git push origin master
 
 cd tool/sync-scripts
